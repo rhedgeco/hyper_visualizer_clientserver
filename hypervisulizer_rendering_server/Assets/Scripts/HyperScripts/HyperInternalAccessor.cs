7c8403780d583e92627ed1184b99ea901831d959
rhedgeco <rhedgeco@gmail.com>
2020-12-10 14:25:11 -0800

    added hyperscripts and internal accessor implementation

---
+++ hypervisulizer_rendering_server/Assets/Scripts/HyperScripts/HyperInternalAccessor.cs
@@ -0,0 +1,34 @@
+﻿using HyperEngine;
+using HyperEngine.Restricted;
+using UnityEngine;
+
+namespace HyperScripts
+{
+    public class HyperInternalAccessor : InternalAccessor
+    {
+        public void SetTime(float time)
+        {
+            SetHyperCoreTime(time);
+        }
+
+        public void SetTotalTime(float time)
+        {
+            SetHyperCoreTotalTime(time);
+        }
+
+        public void InvokeUpdate(HyperValues values)
+        {
+            InvokeFrameUpdate(values);
+        }
+
+        public void PurgeUpdates()
+        {
+            PurgeAllUpdateListeners();
+        }
+
+        public void SetUpPropertyManaging(Transform t)
+        {
+            SetUpPropertyManager(t);
+        }
+    }
+}
