ab8dade42aa078fb1e08706e17ab107e2138d677
rhedgeco <rhedgeco@gmail.com>
2020-12-10 14:19:20 -0800

    Added internal accessor and property manager to engine

---
+++ hyper_engine/HyperEngine/PropertyManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace HyperEngine
+{
+    public class PropertyManager
+    {
+        private static Transform content;
+
+        internal static void SetUpPropertyManager(Transform contentArea)
+        {
+            content = contentArea;
+        }
+
+        // Method is to be overridden by child
+        public static GameObject AddInspectorProperty(RectTransform property)
+        {
+            return Object.Instantiate(property.gameObject, content);
+        }
+    }
+}
---
+++ hyper_engine/HyperEngine/Restricted/InternalAccessor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace HyperEngine.Restricted
+{
+    public abstract class InternalAccessor
+    {
+        protected internal static void SetHyperCoreTime(float time)
+        {
+            HyperCore.Time = time;
+        }
+
+        protected internal static void SetHyperCoreTotalTime(float time)
+        {
+            HyperCore.TotalTime = time;
+        }
+
+        protected internal static void InvokeFrameUpdate(HyperValues values)
+        {
+            HyperCore.UpdateFrame.Invoke(values);
+        }
+
+        protected internal static void PurgeAllUpdateListeners()
+        {
+            HyperCore.UpdateFrame.RemoveAllListeners();
+        }
+
+        protected internal static void SetUpPropertyManager(Transform content)
+        {
+            PropertyManager.SetUpPropertyManager(content);
+        }
+    }
+}
